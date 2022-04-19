4a2e41f69fc2421660c8251986e5d5ab7918b3b3
Rafaela <rafaela.jardim@southsystem.com.br>
2022-04-18 21:53:57 -0300

    Criando a estrutura base da API

---
+++ src/main/java/br/com/southsystem/assembleia/AssembleiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.southsystem.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssembleiaApplication.class, args);
+    }
+
+}
